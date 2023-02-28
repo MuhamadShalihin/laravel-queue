2aeebd84ac1da5dc641106aa262a038da33aa2dc
Shalihin Zaiwadi <shalihin.zaiwadi@gmail.com>
2023-02-28 15:40:21 +0800

    Created a job instance to send reminder email

---
+++ app/Jobs/SendReminderEmail.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\ReminderEmail;
+use Illuminate\Bus\Queueable;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+
+class SendReminderEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $email = new ReminderEmail();
+        Mail::to('shalihin.zaiwadi@gmail.com')->send($email);
+    }
+}
